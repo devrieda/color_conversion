a6936650eda9f352ceed3028a1f9c875f22cfc2d
Derek DeVries <devrieda@gmail.com>
2013-05-10 11:18:53 -0600

    base conversion class

--- lib/color_conversion.rb
+++ lib/color_conversion.rb
@@ -1,5 +1,15 @@
 require 'color_conversion/version'
+require 'color_conversion/errors'
 require 'color_conversion/color'
+require 'color_conversion/color_converter'
+require 'color_conversion/converters/cmyk_converter'
+require 'color_conversion/converters/hex_converter'
+require 'color_conversion/converters/hsl_converter'
+require 'color_conversion/converters/hsv_converter'
+require 'color_conversion/converters/name_converter'
+require 'color_conversion/converters/rgb_converter'
+require 'color_conversion/converters/string_converter'
+require 'color_conversion/converters/null_converter'
 
 module ColorConversion
 end
