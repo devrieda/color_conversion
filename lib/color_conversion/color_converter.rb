8cc82817b33a8e1fb0825a0777542afdb482a84f
Derek DeVries <devrieda@gmail.com>
2013-05-23 10:14:59 -0600

    refactor conversions to clarify intentions of various variables

--- lib/color_conversion/color_converter.rb
+++ lib/color_conversion/color_converter.rb
@@ -33,65 +33,15 @@ module ColorConversion
     end
     
     def hsl
-      r = @rgba[:r] / 255.0
-      g = @rgba[:g] / 255.0
-      b = @rgba[:b] / 255.0
-      min = [r, g, b].min
-      max = [r, g, b].max
-      delta = max - min
+      @r, @g, @b = rgb_array_frac
 
-      h = if max == min
-        0
-      elsif r == max
-        (g - b) / delta 
-      elsif g == max
-        2 + (b - r) / delta 
-      elsif b == max
-        4 + (r - g) / delta
-      end
-      
-      h = [h * 60, 360].min
-      h += 360 if h < 0
-      l = (min + max) / 2.0
-
-      s = if (max == min)
-        0
-      elsif (l <= 0.5)
-        delta / (max + min)
-      else
-        delta / (2.0 - max - min)
-      end
-
-      {h: h.round, s: (s * 100).round, l: (l * 100).round}
+      {h: hue, s: hsl_saturation, l: hsl_lightness}
     end
-    
+
     def hsv
-      r = @rgba[:r].to_f
-      g = @rgba[:g].to_f
-      b = @rgba[:b].to_f
-      min = [r, g, b].min
-      
-      max = [r, g, b].max
-      delta = (max - min).to_f
-      
-      s = max == 0 ? 0 : (delta / max * 1000) / 10.0
-      
-      h = if max == min
-        0
-      elsif r == max
-        (g - b) / delta
-      elsif g == max
-        2 + (b - r) / delta 
-      elsif b == max
-        4 + (r - g) / delta
-      end
-      
-      h = [h * 60, 360].min      
-      h += 360 if h < 0
+      @r, @g, @b = rgb_array
 
-      v = ((max / 255.0) * 1000) / 10.0
-
-      {h: h.round, s: s.round, v: v.round}
+      {h: hue, s: hsv_saturation, v: hsv_value}
     end
     
     def hsb
@@ -101,14 +51,12 @@ module ColorConversion
     end
     
     def cmyk
-      r = @rgba[:r] / 255.0
-      g = @rgba[:g] / 255.0
-      b = @rgba[:b] / 255.0
+      @r, @g, @b = rgb_array_frac
 
-      k = [1.0 - r, 1.0 - g, 1.0 - b].min
-      c = (1.0 - r - k) / (1.0 - k)
-      m = (1.0 - g - k) / (1.0 - k)
-      y = (1.0 - b - k) / (1.0 - k)
+      k = [1.0 - @r, 1.0 - @g, 1.0 - @b].min
+      c = (1.0 - @r - k) / (1.0 - k)
+      m = (1.0 - @g - k) / (1.0 - k)
+      y = (1.0 - @b - k) / (1.0 - k)
 
       {c: (c * 100).round, 
        m: (m * 100).round, 
@@ -122,6 +70,74 @@ module ColorConversion
     
     def name
       NameConverter.name_for_rgb(rgb)
+    end
+    
+    
+    protected
+
+    def rgb_array
+      [@rgba[:r].to_f, @rgba[:g].to_f, @rgba[:b].to_f]
+    end
+    
+    def rgb_array_frac
+      [@rgba[:r] / 255.0, @rgba[:g] / 255.0, @rgba[:b] / 255.0]
+    end
+    
+    def min
+      [@r, @g, @b].min
+    end
+    
+    def max
+      [@r, @g, @b].max
+    end
+    
+    def delta
+      max - min
+    end
+
+    def hue
+      h = if max == min
+        0
+      elsif @r == max
+        (@g - @b) / delta
+      elsif @g == max
+        2 + (@b - @r) / delta
+      elsif @b == max
+        4 + (@r - @g) / delta
+      end
+
+      h = [h * 60, 360].min      
+      h += 360 if h < 0
+      h.round
+    end
+
+    # hsv
+    def hsv_saturation
+      sat = max == 0 ? 0 : (delta / max * 1000) / 10.0
+      sat.round
+    end
+    
+    def hsv_value
+      val = ((max / 255.0) * 1000) / 10.0
+      val.round
+    end
+
+    # hsl
+    def hsl_saturation
+      s = if max == min
+        0
+      elsif hsl_lightness / 100.0 <= 0.5
+        delta / (max + min)
+      else
+        delta / (2.0 - max - min)
+      end
+
+      (s * 100).round
+    end
+    
+    def hsl_lightness
+      light = (min + max) / 2.0 * 100
+      light.round
     end
 
   end
